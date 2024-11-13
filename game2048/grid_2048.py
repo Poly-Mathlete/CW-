9d8d6be3dc4f7e686a6cbcd9cb81055db5d278dc
Nassereddine Abdelhakim <abdelhakim.nassereddine@student-cs.fr>
2024-11-13 23:48:57 +0100

    voici le nouveau main , pull from this one and create new branch for next fonctionnalité

--- game2048/grid_2048.py
+++ game2048/grid_2048.py
@@ -3,36 +3,15 @@ import random
 from typing import List, Tuple, Optional
 
 
-def create_grid() -> List[List[Optional[int]]]:
+def create_grid(n = 4) -> List[List[Optional[int]]]:
     """
-    Crée une grille de jeu 4x4 vide pour le jeu 2048.
-    
+    Crée une grille de jeu (n x n) vide pour le jeu 2048.
+    Args : 
+        n (int) : la taille de la grille par défaut 4 . 
     Returns:
         List[List[Optional[int]]]: Une liste de listes représentant la grille, initialisée avec des valeurs None.
     """
-    return [[None for _ in range(4)] for _ in range(4)]
-
-
-
-
-def grid_add_new_tile_at_position(game_grid: List[List[Optional[int]]], x: int, y: int) -> List[List[Optional[int]]]:
-    """
-    Ajoute la valeur 2 ou 4 à la position donnée dans la grille.
-    
-    Args:
-        game_grid (List[List[Optional[int]]]): La grille de jeu actuelle.
-        x (int): La position x où la nouvelle valeur sera ajoutée.
-        y (int): La position y où la nouvelle valeur sera ajoutée.
-        
-    Returns:
-        List[List[Optional[int]]]: La grille de jeu mise à jour avec le nouveau nombre ajouté.
-    """
-    # Choisir 2 (90% de chance) ou 4 (10% de chance)
-    new_tile_value = 2 if random.random() < 0.9 else 4
-    game_grid[x][y] = new_tile_value
-    
-    return game_grid
-    
+    return [[None for _ in range(n)] for _ in range(n)]
 
 
 def add_new_tile(game_grid: List[List[Optional[int]]]) -> None:
@@ -70,6 +49,7 @@ def get_value_new_tile() -> int:
 
 
 def get_all_tiles(game_grid: List[List[Optional[int]]]) -> List[List[int]]:
+
     """
     Retourne une copie de la grille contenant les valeurs de chaque tuile, où les cases vides (None)
     sont remplacées par 0.
@@ -82,3 +62,62 @@ def get_all_tiles(game_grid: List[List[Optional[int]]]) -> List[List[int]]:
     """
     # Crée une copie de la grille en remplaçant None par 0 pour chaque cellule
     return [[cell if cell is not None else 0 for cell in row] for row in game_grid]
+
+def get_empty_tiles_positions(game_grid: List[List[Optional[int]]]) -> List[Tuple[int, int]]:
+    """
+    Retourne une liste des positions des cases vides (représentées par 0 ou ' ') dans la grille.
+
+    Args:
+        game_grid (List[List[Optional[int]]]): La grille de jeu.
+
+    Returns:
+        List[Tuple[int, int]]: Une liste de tuples représentant les positions (x, y) des cases vides.
+    """
+    empty_positions = []
+    
+    for i in range(len(game_grid)):
+        for j in range(len(game_grid[i])):
+            if game_grid[i][j] == 0 or game_grid[i][j] == ' ':
+                empty_positions.append((i, j))
+    
+    return empty_positions
+
+def grid_get_value(game_grid: List[List[Optional[int]]], x: int, y: int) -> Optional[int]:
+    """
+    Retourne la valeur à la position (x, y) dans la grille de jeu.
+    
+    Args:
+        game_grid (List[List[Optional[int]]]): La grille de jeu.
+        x (int): L'index de la ligne.
+        y (int): L'index de la colonne.
+    
+    Returns:
+        Optional[int]: La valeur de la case à la position (x, y).
+    """
+    return get_all_tiles(game_grid)[x][y]
+
+def get_new_postion(game_grid: List[List[Optional[int]]]) -> List[List[int]]:
+    """
+      Retourne aléatoirement une position vide de la grille.
+    
+    Args:
+        game_grid (List[List[Optional[int]]]): La grille de jeu.
+    
+    Returns:
+        tuple[int, int]: Un tuple représentant les coordonnées (x, y) d'une position vide.
+    """
+    return random.choice(get_empty_tiles_positions(game_grid))
+
+def init_game(n : int ) -> List[List[Optional[int]]]:
+    """
+    Débute le jeux en créant  , la grille et la tuile .
+
+    Args:
+        
+        n (int): La taille de la grille . 
+
+    Returns:
+        List[List[Optional[int]]]: Une liste de listes représentant la grille.
+
+    """
+    get_all_tiles(add_new_tile(create_grid(n)))
